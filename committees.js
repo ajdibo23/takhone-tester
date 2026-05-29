3e0f6b7ed0442c931d5e3c29817a125e5acae76f
AJ DiBasilio <149754393+ajdibo23@users.noreply.github.com>
2026-05-29 00:39:42 -0500

    Add files via upload

---
+++ committees.js
@@ -0,0 +1,242 @@
+
+const committees = [
+    {
+        name: "Communications",
+        description: "This is an example description - it will be filled in soon!",
+        chair: "TBD",
+        contact_email: "TBD(at)oa7.org",
+        committees: [
+            {
+                name: "Website",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Website Building",
+                    "Graphic Design",
+                    "WordPress",
+                    "HTML/CSS",
+                ]
+            },
+            {
+                name: "Publications",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Yearbook",
+                    "Design",
+                    "Writing",
+                    "Graphic Design",
+                    "Logistics",
+                    "Copy Editing",
+                    "Communications",
+                ]
+            },
+            {
+                name: "Social Media",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Graphic Design",
+                    "Social Media Outreach",
+                    "Video Making",
+                    "Writing",
+                ]
+            }
+        ]
+    },
+    {
+        name: "Finance",
+        description: "This is an example description - it will be filled in soon!",
+        chair: "TBD",
+        contact_email: "TBD(at)oa7.org",
+        committees: [
+            {
+                name: "Merchandise",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Graphic Design",
+                    "T-Shirt Design",
+                    "Salesmanship",
+                ]
+            },
+            {
+                name: "Trading Post",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Accounting",
+                    "Finance",
+                    "Design",
+                    "Salesmanship",
+                    "Technology",
+                ]
+            }
+        ]
+    },
+    {
+        name: "Inductions",
+        description: "This is an example description - it will be filled in soon!",
+        chair: "TBD",
+        contact_email: "TBD(at)oa7.org",
+        committees: [
+            {
+                name: "Induction Experience/Luminary",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Leadership",
+                    "Organization",
+                    "Management",
+                    "Friendliness",
+                    "Promotion",
+                ]
+            },
+            {
+                name: "Brotherhood/Nimatting",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Leadership",
+                    "Organization",
+                    "Discussion Leading",
+                    "Friendliness",
+                ]
+            },
+            {
+                name: "Ceremonies",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Public Speaking",
+                    "Memorization",
+                    "Theater",
+                    "Acting",
+                ]
+            },
+            {
+                name: "New Members",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Event Planning",
+                    "Communication",
+                    "Copy Editing",
+                ]
+            }
+        ]
+    },
+    {
+        name: "Operations",
+        description: "This is an example description - it will be filled in soon!",
+        chair: "TBD",
+        contact_email: "TBD(at)oa7.org",
+        committees: [
+            {
+                name: "Unit Elections",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Organization",
+                    "Paperwork Filing",
+                    "Public Speaking",
+                    "Team Management",
+                ]
+            },
+            {
+                name: "OA Unit Representatives",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Management",
+                    "Communication",
+                    "Public Speaking",
+                ]
+            },
+            {
+                name: "Camp Promotions",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Public Speaking",
+                    "Graphic Design",
+                    "Video Making",
+                    "Voiceover",
+                ]
+            },
+            {
+                name: "Performance Measurement Program (PMP)",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Data",
+                    "Promotions",
+                    "Communications",
+                    "Management",
+                    "Public Speaking",
+                ]
+            }
+        ]
+    },
+    {
+        name: "Program",
+        description: "This is an example description - it will be filled in soon!",
+        chair: "TBD",
+        contact_email: "TBD(at)oa7.org",
+        committees: [
+            {
+                name: "Training",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Leadership",
+                    "Public Speaking",
+                    "Design",
+                    "Organization",
+                    "Management",
+                ]
+            },
+            {
+                name: "Camp Service",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Leadership",
+                    "Public Speaking",
+                    "Design",
+                    "Organization",
+                    "Management",
+                ]
+            },
+            {
+                name: "Events",
+                description: "This is an example description - it will be filled in soon!",
+                chair: "TBD",
+                contact_email: "TBD(at)oa7.org",
+                skills: [
+                    "Public Speaking",
+                    "Management",
+                    "Organization",
+                    "Project Planning",
+                    "Event Planning",
+                    "Communication",
+                ]
+            }
+        ]
+    }
+]
---
+++ questions.js
@@ -0,0 +1,220 @@
+const questions = [
+    {
+        question: "What kind of projects sound the most fun to <bold>YOU</bold>?",
+        results: [
+            {
+                answer: "Digital/Graphic Designs (canva, photoshop, etc.)",
+                addSkills: ["Graphic Design", "Website Building", "WordPress", "HTML/CSS", "T-Shirt Design", "Design"]
+            },
+            {
+                answer: "Social Video creation (Instagram, etc.)",
+                addSkills: ["Video Making", "Social Media Outreach", "Communication", "Public Speaking"]
+            },
+            {
+                answer: "Event Planning",
+                addSkills: ["Event Planning", "Communication", "Public Speaking", "Organization", "Management"]
+            },
+            {
+                answer: "Planning Logistics",
+                addSkills: ["Logistics", "Organization", "Management", "Public Speaking", "Communication"]
+            },
+            {
+                answer: "Studying Statistics",
+                addSkills: ["Data", "Promotions", "Communications", "Management", "Public Speaking"]
+            },
+            {
+                answer: "Acting or Theater",
+                addSkills: ["Theater", "Acting", "Public Speaking", "Communication", "Organization", "Management"]
+            }
+        ]
+    },
+    {
+        question: "Which role would you naturally take on when working with a team?",
+        results: [
+            {
+                answer: "The organizer keeping everyone on track",
+                addSkills: ["Organization", "Management", "Team Management"]
+            },
+            {
+                answer: "The creative person making things look good",
+                addSkills: ["Graphic Design", "Design", "Writing"]
+            },
+            {
+                answer: "The presenter or spokesperson",
+                addSkills: ["Public Speaking", "Leadership", "Communication"]
+            },
+            {
+                answer: "The tech/problem-solving person",
+                addSkills: ["Technology", "Data", "Website Building"]
+            }
+        ]
+    },
+    {
+        question: "What best describes you?",
+        results: [
+            {
+                answer: "Creative",
+                addSkills: ["Graphic Design", "Writing", "Design"]
+            },
+            {
+                answer: "Outgoing",
+                addSkills: ["Public Speaking", "Leadership", "Friendliness"]
+            },
+            {
+                answer: "Analytical",
+                addSkills: ["Data", "Finance", "Technology"]
+            },
+            {
+                answer: "Organized",
+                addSkills: ["Organization", "Management", "Paperwork Filing"]
+            }
+        ]
+    },
+    {
+        question: "How do you prefer to communicate with others?",
+        results: [
+            {
+                answer: "Writing emails or posts",
+                addSkills: ["Writing", "Copy Editing", "Communications"]
+            },
+            {
+                answer: "Speaking in front of a group",
+                addSkills: ["Public Speaking", "Leadership", "Communication"]
+            },
+            {
+                answer: "One-on-one conversations",
+                addSkills: ["Friendliness", "Discussion Leading", "Communication"]
+            },
+            {
+                answer: "Through visuals or videos",
+                addSkills: ["Graphic Design", "Video Making", "Social Media Outreach"]
+            }
+        ]
+    },
+    {
+        question: "What would you most enjoy doing at a weekend event?",
+        results: [
+            {
+                answer: "Running a booth or selling something",
+                addSkills: ["Salesmanship", "Finance", "Accounting"]
+            },
+            {
+                answer: "Welcoming and guiding new people",
+                addSkills: ["Friendliness", "Communication", "Event Planning"]
+            },
+            {
+                answer: "Taking photos or making content",
+                addSkills: ["Social Media Outreach", "Video Making", "Graphic Design"]
+            },
+            {
+                answer: "Keeping things running behind the scenes",
+                addSkills: ["Logistics", "Organization", "Paperwork Filing"]
+            }
+        ]
+    },
+    {
+        question: "Which school subject do you enjoy most?",
+        results: [
+            {
+                answer: "English / Language Arts",
+                addSkills: ["Writing", "Copy Editing", "Communication"]
+            },
+            {
+                answer: "Math / Economics",
+                addSkills: ["Accounting", "Finance", "Data"]
+            },
+            {
+                answer: "Computer Science / Technology",
+                addSkills: ["Website Building", "Technology", "WordPress"]
+            },
+            {
+                answer: "Drama / Speech",
+                addSkills: ["Theater", "Acting", "Public Speaking"]
+            }
+        ]
+    },
+    {
+        question: "What do you find most satisfying about completing a project?",
+        results: [
+            {
+                answer: "Seeing the final design or product look great",
+                addSkills: ["Design", "Graphic Design", "T-Shirt Design"]
+            },
+            {
+                answer: "Knowing everything ran smoothly",
+                addSkills: ["Logistics", "Management", "Organization"]
+            },
+            {
+                answer: "Watching an audience react positively",
+                addSkills: ["Public Speaking", "Theater", "Promotion"]
+            },
+            {
+                answer: "Reviewing the data and seeing results",
+                addSkills: ["Data", "Promotions", "Performance Measurement"]
+            }
+        ]
+    },
+    {
+        question: "If you had to lead a meeting, what would you do?",
+        results: [
+            {
+                answer: "Prepare a detailed agenda and keep notes",
+                addSkills: ["Organization", "Paperwork Filing", "Team Management"]
+            },
+            {
+                answer: "Facilitate an open discussion",
+                addSkills: ["Discussion Leading", "Leadership", "Communication"]
+            },
+            {
+                answer: "Create a visual presentation",
+                addSkills: ["Design", "Graphic Design", "Public Speaking"]
+            },
+            {
+                answer: "Delegate tasks and check on progress",
+                addSkills: ["Management", "Team Management", "Leadership"]
+            }
+        ]
+    },
+    {
+        question: "How do you feel about public speaking?",
+        results: [
+            {
+                answer: "I love it — the bigger the crowd, the better",
+                addSkills: ["Public Speaking", "Theater", "Leadership"]
+            },
+            {
+                answer: "I'm okay with it if I'm well prepared",
+                addSkills: ["Public Speaking", "Memorization", "Organization"]
+            },
+            {
+                answer: "I prefer a small group or one-on-one",
+                addSkills: ["Discussion Leading", "Friendliness", "Communication"]
+            },
+            {
+                answer: "I'd rather let my work speak for itself",
+                addSkills: ["Graphic Design", "Writing", "Data"]
+            }
+        ]
+    },
+    {
+        question: "What kind of impact do you most want to make?",
+        results: [
+            {
+                answer: "Grow our organization's reach and reputation",
+                addSkills: ["Promotions", "Social Media Outreach", "Video Making"]
+            },
+            {
+                answer: "Make new members feel welcome and included",
+                addSkills: ["Friendliness", "Event Planning", "Communication"]
+            },
+            {
+                answer: "Improve how the organization operates day-to-day",
+                addSkills: ["Management", "Logistics", "Project Planning"]
+            },
+            {
+                answer: "Create something lasting — a website, yearbook, or video",
+                addSkills: ["Website Building", "Yearbook", "Video Making", "Writing"]
+            }
+        ]
+    }
+];
